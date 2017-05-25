85d5e9a1090a2463521e3d2cff0268c11a6d9776
Folusho Oladipo <folusho.oladipo@andela.com>
2017-05-25 20:36:32 +0100

    rename the app.js file

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.get('*', (req, res) => {
-  res.send('<h1>Hello Lagos! #TIA!</h1>');
+app.get('/api', (req, res) => {
+  res.send('<h1>All clear</h1>');
 });
 
 app.listen(port, () => {
