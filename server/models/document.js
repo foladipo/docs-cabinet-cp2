2aa284246370ab9296538adcee04e8c1a265c17c
Folusho Oladipo <folusho.oladipo@andela.com>
2017-05-29 04:04:58 +0100

    add a model for the Document table

---
+++ server/models/document.js
@@ -0,0 +1,43 @@
+import Sequelize from 'sequelize';
+import initSequelize from '../util/initSequelize';
+
+const sequelize = initSequelize();
+const Document = sequelize.define('Document', {
+  title: {
+    type: Sequelize.STRING
+  },
+  docContent: {
+    type: Sequelize.STRING
+  },
+  access: {
+    type: Sequelize.STRING,
+    validate: {
+      // will only allow letters
+      is: ['[a-z]', 'i'],
+      isIn: {
+        args: [['public', 'private', 'role']],
+        msg: 'You must specify the access level of your'
+          + ' document as either \'public\', \'private\' or \'role\'.'
+      }
+    },
+  },
+  categories: {
+    type: Sequelize.STRING
+  },
+  tags: {
+    type: Sequelize.STRING
+  },
+  createdBy: {
+    type: Sequelize.STRING
+  },
+  createdAt: {
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    type: Sequelize.DATE
+  }
+}, {
+  freezeTableName: true
+});
+
+export default Document;
