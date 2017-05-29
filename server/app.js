d955dcae3bac2ecf94aaa10f839a3da3d14f0e32
Folusho Oladipo <folusho.oladipo@andela.com>
2017-05-29 04:05:04 +0100

    add sample endpoint for creating and retrieving Document's

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import bcryptjs from 'bcryptjs';
 import dotenv from 'dotenv';
 import User from './models/user';
+import Document from './models/document';
 import initSequelize from './util/initSequelize';
 
 dotenv.config();
@@ -46,12 +47,63 @@ app.post('/api/users', (req, res) => {
         username: reqPayload.username,
         password: bcryptjs.hashSync(reqPayload.password, 10),
         roleId: reqPayload.roleId,
-        signUpDate: now.toISOString(),
+        createdAt: now.toISOString(),
         updatedAt: now.toISOString()
       };
       User.create(newUser)
         .then(() => {
           res.json({ message: `Yippe!! User ${newUser.firstName} was created.` });
+        })
+        .catch((err) => {
+          res.json({ message: err.toString() });
+        });
+    })
+    .catch((err) => {
+      res.json({ message: err.toString() });
+    });
+});
+
+app.get('/api/documents', (req, res) => {
+  sequelize.authenticate()
+    .then(() => {
+      Document.sync()
+        .then(() => {
+          Document.findAll()
+          .then((documents) => {
+            res.json({
+              documents
+            });
+          })
+          .catch((err) => {
+            res.json({ message: err.toString() });
+          });
+        })
+        .catch((err) => {
+          res.json({ message: err.toString() });
+        });
+    })
+    .catch((err) => {
+      res.json({ message: err.toString() });
+    });
+});
+
+app.post('/api/documents', (req, res) => {
+  Document.sync()
+    .then(() => {
+      const reqPayload = req.body;
+      const newDocument = {
+        title: reqPayload.title,
+        docContent: reqPayload.docContent,
+        access: reqPayload.access,
+        categories: reqPayload.categories,
+        tags: reqPayload.tags,
+        createdBy: reqPayload.userId,
+        createdAt: now.toISOString(),
+        updatedAt: now.toISOString()
+      };
+      Document.create(newDocument)
+        .then(() => {
+          res.json({ message: `Wawu!! Document '${newDocument.title}' was created.` });
         })
         .catch((err) => {
           res.json({ message: err.toString() });
