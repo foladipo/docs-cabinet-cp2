b91dcedc817f42bfb8f193d94c4353230570966c
Folusho Oladipo <folusho.oladipo@andela.com>
2017-06-01 20:13:24 +0100

    update the length of the salt used to hash passwords

--- server/seeders/02-user.js
+++ server/seeders/02-user.js
@@ -9,8 +9,8 @@ module.exports = {
         lastName: 'Anonymous',
         username: 'foo@example.com',
 
-        // Output, by bcryptjs, of hashSync('Very&&Hard$@', 5).
-        password: '$2a$05$GEmF6Qhjt2u0M4bP2izMAunEMU6HuZmb2rmOwh8EJsyItyH9lWcq2',
+        // Output, by bcryptjs, of hashSync('Very&&Hard$@', 10).
+        password: '$2a$10$jk5F81NcjzUp/F51qShkjus4qUfRSMTFlkMx4l62EPp7teNoVaOyO',
         roleId: 0,
         createdAt: timeStamp,
         updatedAt: timeStamp
