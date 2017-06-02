85b79a5af75c840715c79632ef4119c86a046c90
Folusho Oladipo <folusho.oladipo@andela.com>
2017-06-02 01:00:39 +0100

    add the /api/documents endpoint

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import dotenv from 'dotenv';
 import users from './controllers/users';
+import documents from './controllers/documents';
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.use('/api/users', users);
+app.use('/api/documents', documents);
 
 app.use('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
