024aa2c7523cffd5b33c384ba377d1ab392417e4
Folusho Oladipo <folusho.oladipo@andela.com>
2017-06-08 12:57:40 +0100

    create a middleware that only allows admins to access a given endpoint

---
+++ server/middleware/adminsOnly.js
@@ -0,0 +1,25 @@
+/**
+ * Prevents users that are not at least admins from accessing some endpoints
+ * or routes. If the user is not an admin, it returns a ForbiddenOperationError
+ * response. Otherwise, it calls next().
+ * @param {Request} req - An express Request object with data about the
+ * original request sent to this endpoint e.g the profile of the user making
+ * this request etc.
+ * @param {Response} res - An express Response object with the info this app
+ * will send back to the user e.g error messages like ForbiddenOperationError.
+ * @param {Function} next - The next function or middleware in the callback stack
+ * of express.
+ * @return {void}
+ */
+export default function adminsOnly(req, res, next) {
+  const user = req.decodedUserProfile;
+  const roleId = user.roleId;
+  if (roleId > 0) {
+    next();
+  } else {
+    res.status(403)
+      .json({
+        error: 'ForbiddenOperationError'
+      });
+  }
+}
