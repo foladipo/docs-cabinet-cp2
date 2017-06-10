a51e99d973ed689cba4870f903179fe02084397d
Folusho Oladipo <folusho.oladipo@andela.com>
2017-06-10 10:50:40 +0100

    implement the endpoint for searching for documents i.e GET /api/search/documents?q={docTitle}

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 import users from './controllers/users';
 import documents from './controllers/documents';
+import search from './controllers/search';
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.use('/api/users', users);
 app.use('/api/documents', documents);
+app.use('/api/search', search);
 
 app.use('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
---
+++ server/controllers/search.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import Document from '../models/Document';
+import validateToken from '../middleware/validateToken';
+import getLimitAndOffset from '../util/getLimitAndOffset';
+
+const search = express();
+
+/**
+ * Returns a list of documents whose title match a given query string. The
+ * HTTP request's query parameters can also include a limit and offset for
+ * the search. The limit is the number of documents returned for each
+ * search and the offset is the number of matching results to skip before
+ * collating results (e.g if there are 50 documents that match a query and
+ * an offset of 5 is given, then the list of returned documents will start
+ * from the 6th document in the database). The default limit is 30 and the
+ * default offset is 0. Note that the request must include a query string for
+ * the search. Else, this function sends a DocumentQueryNotSuppliedError response.
+ * @param {Request} req - An express Request object with data about the
+ * original request sent to this endpoint.
+ * @param {Response} res - An express Response object that will contain
+ * the info this app will send back to the user e.g error messages for
+ * failed updates etc.
+ * @return {void}
+ */
+function searchDocuments(req, res) {
+  const userProfile = req.decodedUserProfile;
+  const limitAndOffset = getLimitAndOffset(req.query.limit, req.query.offset);
+  const limit = limitAndOffset.limit;
+  const offset = limitAndOffset.offset;
+  const targetDocumentTitle = req.query.q;
+  if (targetDocumentTitle === undefined || targetDocumentTitle === '') {
+    res.status(400)
+      .json({
+        error: 'DocumentQueryNotSuppliedError'
+      });
+    return;
+  }
+
+  Document
+    .findAll({
+      where: {
+        // NB: There's $iLike too, but right now that's for Postgres alone
+        // and we try to be as SQL dialect agnostic as possible. Besides,
+        // this one, or at least this format, is case-insensitive too.
+        title: { $like: `%${targetDocumentTitle}%` },
+        $or: [
+          { access: 'public' },
+          { access: 'private', createdBy: userProfile.userId }
+        ]
+      },
+      limit,
+      offset
+    })
+    .then((foundDocuments) => {
+      res.status(200).json(foundDocuments);
+    });
+}
+
+search.get('/documents', validateToken, searchDocuments);
+
+export default search;
