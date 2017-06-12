28d422834881038c52b3a97f8d9fd99f9d55b88a
Folusho Oladipo <folusho.oladipo@andela.com>
2017-06-12 17:41:00 +0100

    create a test react component

---
+++ client/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SampleComponent = () => (
+  <h1>This is the sample component.</h1>
+);
+
+ReactDOM.render(<SampleComponent />, document.getElementById('root-container'));
