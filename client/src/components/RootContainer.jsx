6f4bdbb3adc9078df42dcf8edc32442c2042e38b
Folusho Oladipo <folusho.oladipo@andela.com>
2017-06-15 21:01:57 +0100

    add new component

--- client/src/components/RootContainer.jsx
+++ client/src/components/RootContainer.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AuthenticatedRoute from './AuthenticatedRoute';
 import DashboardContainer from './DashboardContainer';
-import LoginContainer from './LoginContainer';
+import AuthenticationPage from './AuthenticationPage';
 
 export default function RootContainer() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={LoginContainer} />
+        <Route exact path="/" component={AuthenticationPage} />
         <AuthenticatedRoute exact path="/dashboard" component={DashboardContainer} />
       </Switch>
     </BrowserRouter>
