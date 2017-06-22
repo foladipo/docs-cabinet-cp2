8cb4024cb3bc9cb258db33169b2325450300c554
Folusho Oladipo <folusho.oladipo@andela.com>
2017-06-22 13:55:21 +0100

    document documentRoute using swagger

--- server/routes/documentsRoute.js
+++ server/routes/documentsRoute.js
@@ -4,13 +4,183 @@ import DocumentsController from '../controllers/DocumentsController';
 import validateToken from '../middleware/validateToken';
 
 const documentsRouter = express.Router();
+/**
+ * @swagger
+ * definitions:
+ *   NewDocument:
+ *     type: object
+ *     required:
+ *        - title
+ *        - docContent
+ *        - access
+ *        - categories
+ *        - tags
+ *     properties:
+ *        title:
+ *           type: string
+ *        docContent:
+ *           type: string
+ *        access:
+ *            type: string
+ *        categories:
+ *            type: string
+ *        tags:
+ *            type: string
+ *   Document:
+ *      allOf:
+ *        - $ref: '#definitions/NewDocument'
+ *        - required:
+ *        - id:
+ *              type: integer
+ *              format: int64
+ */
 documentsRouter.route('/')
+  /**
+   * @swagger
+   * /api/documents:
+   *   get:
+   *      description: Returns a list of all the documents of the user making the request.
+   *      tags:
+   *        - Get Documents List
+   *      produces:
+   *        - application/json
+   *      parameters:
+   *        - name: x-docs-cabinet-authentication
+   *          in: header
+   *          description: An authentication token
+   *          required: true
+   *          type: string
+   *      responses:
+   *        200:
+   *          description: documents
+   *          schema:
+   *            type: array
+   *            items:
+   *              $ref: '#/definitions/NewDocument'
+   */
   .get(validateToken, DocumentsController.getAllDocuments)
+  /**
+   * @swagger
+   * /api/documents:
+   *   post:
+   *     description: Creates a new document
+   *     tags:
+   *      - Create Document
+   *     produces:
+   *      - application/json
+   *     parameters:
+   *       - name: x-docs-cabinet-authentication
+   *         in: header
+   *         description: An authentication token
+   *         required: true
+   *         type: string
+   *       - name: body
+   *         description: Document object
+   *         in:  body
+   *         required: true
+   *         type: string
+   *         schema:
+   *           $ref: '#/definitions/NewDocument'
+   *     responses:
+   *       200:
+   *         description: message
+   *         schema:
+   *          type: string
+   */
   .post(validateToken, DocumentsController.createDocument);
 
 documentsRouter.route('/:id')
+  /**
+   * @swagger
+   * /api/documents/<id>:
+   *   get:
+   *     description: Returns the document identified by id.
+   *     tags:
+   *      - Get document by id
+   *     produces:
+   *      - application/json
+   *     parameters:
+   *        - name: x-docs-cabinet-authentication
+   *          in: header
+   *          description: An authentication token
+   *          required: true
+   *          type: string
+   *        - name: id
+   *          description: Document id
+   *          in:  path
+   *          required: true
+   *          type: integer
+   *     responses:
+   *        200:
+   *          description: documents
+   *          schema:
+   *            type: array
+   *            items:
+   *              $ref: '#/definitions/NewDocument'
+   */
   .get(validateToken, DocumentsController.getDocument)
+  /**
+   * @swagger
+   * /api/documents/<id>:
+   *   put:
+   *     description: Updates the document identified by id.
+   *     tags:
+   *      - Update the document chosen by id.
+   *     produces:
+   *      - application/json
+   *     parameters:
+   *        - name: x-docs-cabinet-authentication
+   *          in: header
+   *          description: An authentication token
+   *          required: true
+   *          type: string
+   *        - name: id
+   *          description: Document id
+   *          in:  path
+   *          required: true
+   *          type: integer
+   *        - name: body
+   *          description: Document object
+   *          in:  body
+   *          required: true
+   *          type: string
+   *          schema:
+   *            $ref: '#/definitions/NewDocument'
+   *     responses:
+   *        200:
+   *          description: message
+   *          schema:
+   *            type: string
+   */
   .put(validateToken, DocumentsController.updateDocument)
+  /**
+   * @swagger
+   * /api/document/<id>
+   *    delete:
+   *      description: Deletes the document identified by id.
+   *      tags:
+   *        - Delete document
+   *      produces:
+   *        - application/json
+   *      parameters:
+   *        - name: x-docs-cabinet-authentication
+   *          in: header
+   *          description: An authentication token
+   *          required: true
+   *          type: string
+   *        - name: id
+   *          description: Document id
+   *          in:  path
+   *          required: true
+   *          type: integer
+   *      responses:
+   *        200:
+   *          description: users
+   *          schema:
+   *            type: array
+   *            items:
+   *              $ref: '#/definitions/Document'
+   */
   .delete(validateToken, DocumentsController.deleteDocument);
 
 export default documentsRouter;
