2bf42e572de54f67d89ecb26216b3aaf3d2ab615
Folusho Oladipo <folusho.oladipo@andela.com>
2017-06-26 09:39:17 +0100

    update the store's initial state

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -4,15 +4,26 @@ import reducers from '../reducers';
 
 const middleware = applyMiddleware(thunk);
 
+const token = window.localStorage.getItem('token');
+const userString = window.localStorage.getItem('user');
+let isLoggedIn = false;
+const isLoggingIn = false;
+const isLoggingOut = false;
+let user = {};
+if (token) {
+  isLoggedIn = true;
+  user = JSON.parse(userString);
+}
+
 const store = createStore(
   reducers,
   {
     user: {
-      isLoggedIn: false,
-      isLoggingIn: false,
-      isLoggingOut: false,
-      token: null,
-      user: {},
+      isLoggedIn,
+      isLoggingIn,
+      isLoggingOut,
+      token,
+      user,
       signUpError: '',
       loginError: ''
     },
