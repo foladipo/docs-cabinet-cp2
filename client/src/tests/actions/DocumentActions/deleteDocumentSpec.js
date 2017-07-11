9e6bf1527bb89b776cdbae59179d911a786cfe3a
Folusho Oladipo <folusho.oladipo@andela.com>
2017-07-11 19:04:36 +0100

    add specs for the action for deleting a document

---
+++ client/src/tests/actions/DocumentActions/deleteDocumentSpec.js
@@ -0,0 +1,57 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import * as ActionTypes from '../../../constants';
+import MockHttpClient from '../../MockHttpClient';
+import MockHttpErrorClient from '../../MockHttpErrorClient';
+import { deleteDocument } from '../../../actions/DocumentActions';
+
+const expect = chai.expect;
+
+describe('deleteDocument', () => {
+  it('should dispatch its success actions correctly', () => {
+    const token = 'RANDOM_TOKEN';
+    const targetDocumentId = 12;
+    const deleteDocumentActions = deleteDocument(token, targetDocumentId);
+    const spy = sinon.spy();
+    deleteDocumentActions(spy, MockHttpClient);
+    expect(spy.calledTwice).to.equal(true);
+    expect(spy.calledWith(
+      {
+        type: ActionTypes.DELETE_DOCUMENT_PENDING,
+        payload: { targetDocumentId }
+      }
+    )).to.equal(true);
+    expect(spy.calledWith(
+      {
+        type: ActionTypes.DELETE_DOCUMENT_FULFILLED,
+        payload: {
+          message: 'Request successful.'
+        }
+      }
+    )).to.equal(true);
+  });
+
+  it('should dispatch its failure actions correctly', () => {
+    const token = 'RANDOM_TOKEN';
+    const targetDocumentId = 12;
+    const deleteDocumentActions = deleteDocument(token, targetDocumentId);
+    const spy = sinon.spy();
+    deleteDocumentActions(spy, MockHttpErrorClient);
+    expect(spy.calledTwice).to.equal(true);
+    expect(spy.calledWith(
+      {
+        type: ActionTypes.DELETE_DOCUMENT_PENDING,
+        payload: { targetDocumentId }
+      }
+    )).to.equal(true);
+    expect(spy.calledWith(
+      {
+        type: ActionTypes.DELETE_DOCUMENT_REJECTED,
+        payload: {
+          targetDocumentId,
+          message: 'Request failed.'
+        }
+      }
+    )).to.equal(true);
+  });
+});
