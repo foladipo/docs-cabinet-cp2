171089dca8f57c1645181ff819d617e23a4aa67a
Folusho Oladipo <folusho.oladipo@andela.com>
2017-07-11 18:00:15 +0100

    add specs for the action for creating a document

---
+++ client/src/tests/actions/DocumentActions/createDocumentSpec.js
@@ -0,0 +1,57 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import * as ActionTypes from '../../../constants';
+import MockHttpClient from '../../MockHttpClient';
+import MockHttpErrorClient from '../../MockHttpErrorClient';
+import { createDocument } from '../../../actions/DocumentActions';
+
+const expect = chai.expect;
+
+describe('createDocument', () => {
+  it('should dispatch its success actions correctly', () => {
+    const token = 'RANDOM_TOKEN';
+    const title = 'Foo';
+    const content = 'Lorem ipsum';
+    const access = 'public';
+    const categories = 'fiction';
+    const tags = 'random';
+    const createDocumentActions =
+      createDocument(token, title, content, access, categories, tags);
+    const spy = sinon.spy();
+    createDocumentActions(spy, MockHttpClient);
+    expect(spy.calledTwice).to.equal(true);
+    expect(spy.calledWith(
+      {
+        type: ActionTypes.CREATE_DOCUMENT_PENDING
+      }
+    )).to.equal(true);
+    expect(spy.calledWith(
+      {
+        type: ActionTypes.CREATE_DOCUMENT_FULFILLED,
+        payload: {
+          message: 'Request successful.'
+        }
+      }
+    )).to.equal(true);
+  });
+
+  it('should dispatch its failure actions correctly', () => {
+    const createDocumentActions = createDocument();
+    const spy = sinon.spy();
+    createDocumentActions(spy, MockHttpErrorClient);
+    expect(spy.calledTwice).to.equal(true);
+    expect(spy.calledWith(
+      {
+        type: ActionTypes.CREATE_DOCUMENT_PENDING
+      }
+    )).to.equal(true);
+    expect(spy.calledWith(
+      {
+        type: ActionTypes.CREATE_DOCUMENT_REJECTED,
+        payload: {
+          message: 'Request failed.'
+        }
+      }
+    )).to.equal(true);
+  });
+});
